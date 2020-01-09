45d01eab122399a9854a9736ef77611e3c7f91e4
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-09 10:24:41 -0800

    Feature Task 2 Complete.

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 - Start time: 10:45 AM
 - Finish time: 11:55 AM
 - Actual time needed to complete: 1 hour and 10 minutes.
+
+#### Feature #2: Filter Images
+
+- Estimated time to complete: ~ 45 minutes.
+- Start time: 10:00 AM
+- Finish time: 10:20 AM
+- Actual time needed to complete: 20 minutes.
+
+#### Feature #3: Style The Application
+
+- Estimated time to complete:
+- Start time:
+- Finish time:
+- Actual time needed to complete:
--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 'use strict';
-
 // Declare array to hold Image instances.
 const allImages = [];
 
+// Create array to hold object keywords for <select> element.
+const allOptions = [];
+
 // Define Image constructor function.
 function Image(newImg) {
-  this.image_url = newImg.image_url;
+  this.imageUrl = newImg.image_url;
   this.title = newImg.title;
   this.description = newImg.description;
   this.keyword = newImg.keyword;
@@ -13,18 +15,27 @@ function Image(newImg) {
   allImages.push(this);
 }
 
+Image.prototype.newOption = function() {
+  const selectEl = $('#select');
+  if (!allOptions.includes(this.keyword)) {
+    allOptions.push(this.keyword);
+    selectEl.append(`<option value="${this.keyword}">${this.keyword}</option>`);
+  }
+};
+
 Image.prototype.render = function() {
   // Grab html template used to render Image objects to page.
   const photoTemplate = $('#photo-template').html();
 
   // Create a new <section> element.
   const $newSection = $('<section></section>');
+  $newSection.addClass(this.keyword);
 
   $newSection.html(photoTemplate);
 
   // Append the Image instance data to the new <section> element.
   $newSection.find('h2').text(this.title);
-  $newSection.find('img').attr('src', this.image_url);
+  $newSection.find('img').attr('src', this.imageUrl);
   $newSection.find('img').attr('alt', this.title);
   $newSection.find('p').text(this.description);
 
@@ -32,9 +43,24 @@ Image.prototype.render = function() {
   $('main').append($newSection);
 };
 
-$.ajax('./data/page-1.json', { method: 'GET', dataType: 'JSON' }).then(data => {
-  data.forEach(img => {
-    let newImg = new Image(img);
-    newImg.render();
-  });
+function displayImage() {
+  $('section').hide();
+  let selectedOption = $(this).val();
+  $(`.${selectedOption}`).show();
+}
+
+$(document).ready(() => {
+  // Grab data from a local file and run through Image constructor.
+  // eslint-disable-next-line comma-dangle
+  $.ajax('./data/page-1.json', { method: 'GET', dataType: 'JSON' }).then(
+    data => {
+      data.forEach(img => {
+        let newImage = new Image(img);
+        newImage.newOption();
+        newImage.render();
+      });
+    }
+  );
+
+  $('select').on('change', displayImage);
 });
